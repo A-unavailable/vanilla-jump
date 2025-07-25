c99ba67139919b058121d005309ab95815b5b5df
A-unavailable <immnazz9@gmail.com>
2025-07-25 14:29:52 +0200

    Update NotificationReceiver.java

--- app/src/main/java/ch/blinkenlights/android/vanilla/NotificationReceiver.java
+++ app/src/main/java/ch/blinkenlights/android/vanilla/NotificationReceiver.java
@@ -1,1 +1,15 @@
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if ("ACTION_JUMP_FORWARD".equals(intent.getAction())) {
+            // Вызовите метод для перепрыгивания на 10 секунд
+            PlaybackService playbackService = new PlaybackService();
+            playbackService.jumpForward();
+        }
+    }
+}
 
